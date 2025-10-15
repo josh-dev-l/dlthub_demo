30279dd9c6874810f48dde048b42b9e054b731b6
Joshua Levin <joshua.levin@rmb.co.za>
2025-10-15 22:00:56 +0200

    added new datasets

--- ingestion/s3_to_iceberg.py
+++ ingestion/s3_to_iceberg.py
@@ -3,12 +3,21 @@ from dlt.sources.filesystem import filesystem, read_csv
 
 BRONZE_LAYER = "cat_rnd_odp_dev_lake_bronze"
 
-tpch_source_s3 = filesystem(
+tpch_region_source_s3 = filesystem(
     file_glob="h/1/region*.tbl"
 )
 
-reader  = (tpch_source_s3 | read_csv(delimiter="|", header=None, names=["r_regionkey", "r_name", "r_comment"]))
+tpch_nation_source_s3 = filesystem(
+    file_glob="h/100/nation*.tbl*"
+)
 
+tpch_customer_source_s3 = filesystem(
+    file_glob="h/100/customer*.tbl*"
+)
+
+region  = (tpch_region_source_s3 | read_csv(delimiter="|", header=None, names=["r_regionkey", "r_name", "r_comment"]))
+nation = (tpch_nation_source_s3 | read_csv(delimiter="|", header=None, names=["n_nationkey", "n_name", "n_regionkey", "n_comment"]))
+customer = (tpch_customer_source_s3 | read_csv(delimiter="|", header=None, names=["c_custkey", "c_name", "c_address", "c_nationkey", "c_phone", "c_acctbal", "c_mktsegment", "c_comment"]))
 
 pipeline = dlt.pipeline(
     pipeline_name="tpch_athena_pipeline", 
@@ -17,5 +26,9 @@ pipeline = dlt.pipeline(
     staging="filesystem"
     )
 
-info = pipeline.run(reader, table_name='tpch_region')
-print(info)
+nation = pipeline.run(nation, table_name='tpch_nation')
+customer = pipeline.run(customer, table_name='tpch_customer')
+region = pipeline.run(region, table_name='tpch_region')
+print(nation)
+print(customer)
+print(region)
